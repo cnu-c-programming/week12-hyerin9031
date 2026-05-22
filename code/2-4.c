27cab17e5854f8428fb1c4f07728b326da1b8d03
hyerin9031 <148173524+hyerin9031@users.noreply.github.com>
2026-05-22 10:26:57 +0900

    Update 2-4.c

--- code/2-4.c
+++ code/2-4.c
@@ -18,24 +18,4 @@ int main(int argc, const char* argv[]) {
     }
 
     fclose(fp);
-}#include <stdio.h>
-
-int main(int argc, const char* argv[]) {
-    if (argc < 2)
-        return 0;
-
-    FILE* fp = fopen(argv[1], "r");
-
-    int arr[26] = {0};
-    while(!feof(fp)){
-        char c;
-        fscanf(fp, "%c", &c);
-        arr[(int)c - 97] += 1;
-    }
-
-    for(int i=0; i<26; i++){
-        printf("%c: %d\n",(char)(i+97), arr[i]);
-    }
-
-    fclose(fp);
 }
